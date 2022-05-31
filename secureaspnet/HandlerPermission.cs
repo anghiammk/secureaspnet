921cfb40e60118296d15512adf03ce0d9d50612f
anghiammk <anhnghiammk@gmail.com>
2022-05-31 21:58:01 +0700

    add LogonUser using another handle

---
+++ secureaspnet/HandlerPermission.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Security.Permissions;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+
+namespace secureaspnet
+{
+    public class HandlerPermission
+    {
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
+        int dwLogonType, int dwLogonProvider, out SafeTokenHandle phToken);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public extern static bool CloseHandle(IntPtr handle);
+
+       
+        [PermissionSetAttribute(SecurityAction.Demand, Name = "FullTrust")]
+        public static void Main(string[] args)
+        {
+            SafeTokenHandle safeTokenHandle;
+            try
+            {
+                string userName, domainName, password;
+                domainName = Console.ReadLine();
+                userName = Console.ReadLine();
+                password = Console.ReadLine();
+                const int LOGON32_PROVIDER_DEFAULT = 0;
+                const int LOGON32_LOGON_INTERACTIVE = 2;
+
+                // Call LogonUser to obtain a handle to an access token.
+                bool returnValue = LogonUser(userName, domainName, password,
+                    LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT,
+                    out safeTokenHandle);
+
+                if (false == returnValue)
+                {
+                    int ret = Marshal.GetLastWin32Error();
+                    throw new System.ComponentModel.Win32Exception(ret);
+                }
+                using (safeTokenHandle)
+                {
+                    Console.WriteLine("Before impersonation: "
+                        + WindowsIdentity.GetCurrent().Name);
+                 
+                    using (WindowsIdentity newId = new WindowsIdentity(safeTokenHandle.DangerousGetHandle()))
+                    {
+                        using (WindowsImpersonationContext impersonatedUser = newId.Impersonate())
+                        {
+                            Console.WriteLine("After impersonation: "
+                                + WindowsIdentity.GetCurrent().Name);
+                        }
+                    }
+                    Console.WriteLine("After closing the context: " + WindowsIdentity.GetCurrent().Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred. " + ex.Message);
+            }
+        }
+    }
+
+    public sealed class SafeTokenHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        private SafeTokenHandle()
+            : base(true)
+        {
+        }
+
+        [DllImport("kernel32.dll")]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        protected override bool ReleaseHandle()
+        {
+            return CloseHandle(handle);
+        }
+    }
+}
